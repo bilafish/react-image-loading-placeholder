df4b7f50354d1fd3ff7ff6e35d46642dfeb4a395
Jason <bilafish@gmail.com>
2021-02-24 12:11:51 +0800

    tweak: add skeleton placeholder

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import ReactPlaceholder from "react-placeholder";
 const ImageLoader = ({ imgSrc }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   console.log(isLoaded);
@@ -9,7 +9,13 @@ const ImageLoader = ({ imgSrc }) => {
 
   return (
     <>
-      {!isLoaded && <p>Loading...</p>}
+      {!isLoaded && (
+        <ReactPlaceholder
+          type="media"
+          ready={isLoaded}
+          style={{ height: 333.328, width: 500 }}
+        ></ReactPlaceholder>
+      )}
       <img
         src={imgSrc}
         alt=""
